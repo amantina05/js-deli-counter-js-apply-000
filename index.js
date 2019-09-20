4139df1255dc3888ed610e16b48edc76baf0f787
Amantina Sosa <amantinasosa@gmail.com>
2019-09-20 16:47:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function nowServing (katzDeliLine) {
   if(katzDeliLine.length === 0){
     return 'There is nobody waiting to be served!'
   }
-  if(katzDeliLine.length < 1 ){
+  
     let newL = katzDeliLine.shift()
     return 'Currently serving ' + newL + '.'
-  }
+  
 }
 
 
