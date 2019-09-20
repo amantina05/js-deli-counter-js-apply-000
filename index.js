4b266b9e81cfcd36176a22f8f5d26a76609d84af
Amantina Sosa <amantinasosa@gmail.com>
2019-09-20 16:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ function takeANumber(currPeople, newPerson){
 }
 
 
-function nowServing (katzDeliLine) {
-  if(katzDeliLine.length === 0){
-    return 'There is nobody waiting to be served!'
-  }
-  if(katzDeliLine.length < 1 ){
-    let newL = katzDeliLine.shift()
-    return 'Currently serving ' + newL + '.'
-  }
-}
+// function nowServing (katzDeliLine) {
+//   if(katzDeliLine.length === 0){
+//     return 'There is nobody waiting to be served!'
+//   }
+//   if(katzDeliLine.length < 1 ){
+//     let newL = katzDeliLine.shift()
+//     return 'Currently serving ' + newL + '.'
+//   }
+// }
 
 
-function currentLine (line){
-  if (line.length === 0){
-    return 'The line is currently empty.'
-  }
-  let newArr = []
-  for(let i = 0; i< line.length; i++){
-    if(line[i] < 1){
-      newArr.push(i + 1 + '.' + line[i])
-    }
-    return "The line is currently: " + newArr.join(", ");
-  }
-}
+// function currentLine (line){
+//   if (line.length === 0){
+//     return 'The line is currently empty.'
+//   }
+//   let newArr = []
+//   for(let i = 0; i< line.length; i++){
+//     if(line[i] < 1){
+//       newArr.push(i + 1 + '.' + line[i])
+//     }
+//     return "The line is currently: " + newArr.join(", ");
+//   }
+// }
 
