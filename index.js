11d8734fb4b233e9300747be9332061189551153
Amantina Sosa <amantinasosa@gmail.com>
2019-09-20 16:48:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ function nowServing (katzDeliLine) {
 }
 
 
-// function currentLine (line){
-//   if (line.length === 0){
-//     return 'The line is currently empty.'
-//   }
-//   let newArr = []
-//   for(let i = 0; i< line.length; i++){
-//     if(line[i] < 1){
-//       newArr.push(i + 1 + '.' + line[i])
-//     }
-//     return "The line is currently: " + newArr.join(", ");
-//   }
-// }
+function currentLine (line){
+  if (line.length === 0){
+    return 'The line is currently empty.'
+  }
+  let newArr = []
+  for(let i = 0; i< line.length; i++){
+    if(line[i] < 1){
+      newArr.push(i + 1 + '.' + line[i])
+    }
+    return "The line is currently: " + newArr.join(", ");
+  }
+}
 
