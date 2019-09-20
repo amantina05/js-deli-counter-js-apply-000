a9a701f55edc8cd18885ca0fd421e650b197f3c8
Amantina Sosa <amantinasosa@gmail.com>
2019-09-20 16:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,35 +6,34 @@ function takeANumber(currPeople, newPerson){
   return newArr
 }
 
+// describe('deli', () => {
+//   describe('takeANumber', () => {
+//     var katzDeli;
+//     var otherDeli;
 
-describe('deli', () => {
-  describe('takeANumber', () => {
-    var katzDeli;
-    var otherDeli;
+//     beforeEach(() => {
+//       katzDeli = [];
+//       otherDeli = ["Steven", "Blake", "Avi"];
+//     });
 
-    beforeEach(() => {
-      katzDeli = [];
-      otherDeli = ["Steven", "Blake", "Avi"];
-    });
+//     it('adds a person to the line', () => {
+//       expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
+//       expect(katzDeli).toEqual(['Ada']);
+//     });
 
-    it('adds a person to the line', () => {
-      expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, Ada. You are number 1 in line.");
-      expect(katzDeli).toEqual(['Ada']);
-    });
+//     it('appends the person the end of the line if there are already people on it', () => {
+//       expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
+//       expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
+//     });
 
-    it('appends the person the end of the line if there are already people on it', () => {
-      expect(takeANumber(otherDeli, 'Grace')).toEqual("Welcome, Grace. You are number 4 in line.");
-      expect(otherDeli).toEqual(["Steven", "Blake", "Avi", "Grace"]);
-    });
+//     it("properly handles multiple people being added", () => {
+//       takeANumber(katzDeli, 'Ada');
+//       takeANumber(katzDeli, 'Grace');
+//       takeANumber(katzDeli, 'Kent');
 
-    it("properly handles multiple people being added", () => {
-      takeANumber(katzDeli, 'Ada');
-      takeANumber(katzDeli, 'Grace');
-      takeANumber(katzDeli, 'Kent');
-
-      expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
-    });
-  });
+//       expect(katzDeli).toEqual(["Ada", "Grace", "Kent"]);
+//     });
+//   });
   
 // 1. Build a function that a new customer will use when entering the deli. The function, `takeANumber`, should accept two paramaters: the current line of people, along with the new person's name. The function should return a welcome message including the new person's position in line, such as `"Welcome, Ada. You are number 1 in line."`. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 
@@ -74,15 +73,15 @@ function currentLine (line){
   }
   
 }
-  describe('currentLine(line)', () => {
-    it('returns "The line is currently empty." if no one is in line', () => {
-      expect(currentLine([])).toEqual("The line is currently empty.");
-    });
+//   describe('currentLine(line)', () => {
+//     it('returns "The line is currently empty." if no one is in line', () => {
+//       expect(currentLine([])).toEqual("The line is currently empty.");
+//     });
 
-    it('says who is in line when there are people waiting', () => {
-      expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
-    });
-  });
-})
+//     it('says who is in line when there are people waiting', () => {
+//       expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
+//     });
+//   });
+// })
 
 // 3. Build a function `currentLine` that accepts the current line of people and returns the current line as a string; for example, if 'katzDeliLine' is currently `["Ada", "Grace"]`, `currentLine(katzDeliLine)` would return `"The line is currently: 1. Ada, 2. Grace"`.  You don't have to use `katzDeliLine` as a variable or parameter name in your function though, it's just an example of a variable that might be passed to it. If there is nobody in line, it should return `"The line is currently empty."`
